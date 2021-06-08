8e62977f6023be1a5a4ceac87ea02394252634ff
abhisofriendly <abhisofriendly@gmail.com>
2021-06-08 16:44:55 +0530

    simple card bouncing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { Card, Button, Spinner } from 'react-bootstrap'
+import React, { useState } from 'react';
+import { DATA_SET } from './dataset'
 
-function App() {
+const App = () => {
+  const [testArr, setTestArr] = useState(DATA_SET)
+
+  const [loading, setLoading] = useState(false)
+
+  const buttonCLick = (idx) => {
+    const newData = testArr.find((e) => e.index === idx)
+    const restData = testArr.filter((e) => e.index !== idx)
+
+    if (JSON.stringify(restData.concat([newData])) !== JSON.stringify(testArr)) {
+      setLoading(true)
+      setTestArr(restData.concat([newData]))
+      setTimeout(() => {
+        setLoading(false)
+      }, 1000)
+    }
+  }
+
+  const renderFunction = (el, idx) => (
+    <Card style={{ width: '18rem', margin: 'auto', marginTop: idx * 30 }} onClick={() => buttonCLick(el.index)} className={"active"} key={el.index}>
+      <Card.Img variant="top" src={el.imageUrl} />
+      <Card.Body>
+        <Card.Title>{el.name}</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the bulk of
+            the card's content from card {el.index}.
+            </Card.Text>
+        <Button variant="primary">click on card {el.index}</Button>
+      </Card.Body>
+    </Card>
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading && <Spinner animation="border" role="status" className="spinner_class" variant="light">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+      }
+      {
+        testArr?.map((el, idx) => (
+          renderFunction(el, idx)
+        ))
+      }
     </div>
   );
 }
---
+++ src/dataset.js
@@ -0,0 +1,38 @@
+export const DATA_SET = [
+    {
+      name: 'test1',
+      email: 'test1@mail.com',
+      index: 1,
+      imageUrl: 'https://i.picsum.photos/id/777/200/200.jpg?hmac=ztlrKXJq3EjICHZm7VdNUEexuiMEGRpOrp-QWzBRYng'
+    },
+    {
+      name: 'test2',
+      email: 'test2@mail.com',
+      index: 2,
+      imageUrl: 'https://i.picsum.photos/id/556/200/200.jpg?hmac=5uOJ4fW7ElE2P5NfHlvz2zx4d99Ts2-lxy8tucygHLc'
+    },
+    {
+      name: 'test3',
+      email: 'test3@mail.com',
+      index: 3,
+      imageUrl: 'https://i.picsum.photos/id/212/200/200.jpg?hmac=U4JUx4PJyTuKdZEPAk2Cw01YZM8rOypF8fTTO39POko'
+    },
+    {
+        name: 'test4',
+        email: 'test5@mail.com',
+        index: 4,
+        imageUrl: 'https://i.picsum.photos/id/451/200/200.jpg?hmac=ZxfTfjtPhONiVjaPd1HG3lQuTRy_puiwOF6qStaMe7g'
+      },
+      {
+        name: 'test5',
+        email: 'test5@mail.com',
+        index: 5,
+        imageUrl: 'https://i.picsum.photos/id/449/200/200.jpg?hmac=FD7uqDWwU1CeTIaCQGi9nY0XGVRtSV7cnadWYqPt0CU'
+      },
+      {
+        name: 'test6',
+        email: 'test6@mail.com',
+        index: 6,
+        imageUrl: 'https://i.picsum.photos/id/923/200/200.jpg?hmac=3VHvOqFmO1AmGdpW-XcIVVb5CSOm5AwgyYRt9jYWAvo'
+      } 
+  ]
